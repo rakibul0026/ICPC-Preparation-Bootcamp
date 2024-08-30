d41534a2750450c60238ec945fcd7e10cf199e81
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-30 11:58:29 +0600

    Update Sort the string in ascending order.cpp

--- String/0.string some opration/Sort Numeric  string/Sort the string in ascending order.cpp
+++ String/0.string some opration/Sort Numeric  string/Sort the string in ascending order.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+  string s = "3568393839857";
+    
+    // Sort the string in ascending order
+    sort(s.begin(), s.end());
+    cout << s << endl; // Output: 3333557888899
 
+   
+    
+
+}
