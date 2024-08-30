687284f346675905054b5f5b168f5ea1d8ee264b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-31 00:23:31 +0600

    Update String Right rotation.cpp

--- String/3.Left Rotation and Right Rotation of a string /String Right rotation.cpp
+++ String/3.Left Rotation and Right Rotation of a string /String Right rotation.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string right_rotation(string s)
+{
+
+    string tem;
+   
+    for (int i = 1; i <s.size(); i++)
+    {
+        tem += s[i];
+    }
+     tem += s.front();
+
+    return tem;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    string ans = right_rotation(s);
+    cout << ans << endl;
+    return 0;
+}
 
