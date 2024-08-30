3934d8df8ef505bbe141e04815f0f1b0efb393ca
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-30 11:59:11 +0600

    Update Sort the string in descending order.cpp

--- String/0.string some opration/Sort Numeric  string/Sort the string in descending order.cpp
+++ String/0.string some opration/Sort Numeric  string/Sort the string in descending order.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+  string s = "3568393839857";
+  
 
+    // Sort the string in descending order
+    sort(s.begin(), s.end(), greater<char>());
+    
+    cout << s << endl; // Output: 9988887553333
+    
+
+}
