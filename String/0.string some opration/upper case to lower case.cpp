508954145bb48ef09c639e2c8aabdd268aedff2b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-30 11:29:46 +0600

    Update upper case to lower case.cpp

--- String/0.string some opration/upper case to lower case.cpp
+++ String/0.string some opration/upper case to lower case.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string  str="adsfgshskanakanjs";
 
+    //convert into upper case
+
+    for(int i=0;i<str.size();i++){
+        if(str[i]>='a'&& str[i]<='z')
+        str[i]-=32;
+    }
+
+    cout<<str<<endl;
+
+    //convert to  lower case
+
+    string s2="ASDFHGJGKICC";
+
+    for(int i=0;i<str.size();i++){
+        if(str[i]>='A'&& str[i]<='Z')
+        str[i]+=32;
+    }
+
+    cout<<str<<endl;
+
+
+}
