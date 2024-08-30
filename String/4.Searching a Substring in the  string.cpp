d02da36d8303a06e454633179f690ae5d603d6c2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-31 00:24:18 +0600

    Update 4.Searching a Substring in the  string.cpp

--- String/4.Searching a Substring in the  string.cpp
+++ String/4.Searching a Substring in the  string.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void search(string s1,string subs)
+{
+    
+    int a = s1.find(subs);
+    if (a>=0)
+    {
+        cout << "True" << endl;
+    }
+    else
+        cout <<"False" << endl;
+}
+
+int main()
+{
+    string s1,subs;
+    cout<<"Enter the String"<<endl;
+    cin>>s1;
+
+    cout<<"Enter the sub_String"<<endl;
+    cin>>subs;
+
+    search(s1,subs);
+
+    
+}
 
