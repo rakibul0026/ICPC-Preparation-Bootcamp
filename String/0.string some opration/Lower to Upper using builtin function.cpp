c6198c640bfe70cbc46c3eb450cd7de4042a876f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-30 11:46:32 +0600

    Update Lower to Upper using builtin function.cpp

--- String/0.string some opration/Lower to Upper using builtin function.cpp
+++ String/0.string some opration/Lower to Upper using builtin function.cpp
@@ -1,1 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s="njfhwyronxkslairdf";
 
+    transform(s.begin(),s.end(),s.begin() ,:: toupper);
+    cout<<s<<endl;
+}
