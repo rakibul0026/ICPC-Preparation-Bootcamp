b38d384941cb08b79ff1d62401e9ce108322f1d5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-30 11:32:32 +0600

    Update String find, size,length,substr,sort.cpp

--- String/0.string some opration/String find, size,length,substr,sort.cpp
+++ String/0.string some opration/String find, size,length,substr,sort.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main(){
+   /* string s1 = "nincompoop";
+   cout << s1.find("com") << endl;
+
+   cout << s1.size() << endl;
+   cout << s1.length() << endl;
+
+   string s=s1.substr(2,8);
+   cout<<s;*/
+
+   //sorting a string
+   string s3="nvmbnjwiwdnc";
+   sort(s3.begin(),s3.end());
+   cout<<s3;
+
+    return 0;
+}
