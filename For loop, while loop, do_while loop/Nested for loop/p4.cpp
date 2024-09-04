87dd9a90cc2ef6d0e029c03b2dbc5856992a32eb
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 12:55:25 +0600

    Create p4.cpp

---
+++ For loop, while loop, do_while loop/Nested for loop/p4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+           
+            x++;
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
