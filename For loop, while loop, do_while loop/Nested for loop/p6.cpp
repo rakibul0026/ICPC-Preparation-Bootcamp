4eb715ffccd5ca0099794d4bc550870c5db56682
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 13:01:47 +0600

    Update p6.cpp

--- For loop, while loop, do_while loop/Nested for loop/p6.cpp
+++ For loop, while loop, do_while loop/Nested for loop/p6.cpp
@@ -1,1 +1,20 @@
 
+/*pattern be liked
+3 6 9 12 15 
+18 21 24 27 30
+33 36 39 42 45
+48 51 54 57 60
+63 66 69 72 75 */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+           
+            x=x+3;
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
