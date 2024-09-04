a3e5e1dc26880f3deb39994637b535925faf5f5d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 11:02:39 +0600

    Update print all Even number using continue keyword.cpp

--- For loop, while loop, do_while loop/print all Even number using continue keyword.cpp
+++ For loop, while loop, do_while loop/print all Even number using continue keyword.cpp
@@ -1,1 +1,13 @@
 
+//print all Even number using continue keyword
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    for(int i=0;i<=20;i++){
+        if(i%2==1)
+        continue;
+        cout<<i<<" ";
+    }
+}
