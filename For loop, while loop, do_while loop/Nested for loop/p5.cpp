c111bfbd250201707f995b5a772791fbc1892b72
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 13:00:13 +0600

    Update p5.cpp

--- For loop, while loop, do_while loop/Nested for loop/p5.cpp
+++ For loop, while loop, do_while loop/Nested for loop/p5.cpp
@@ -1,1 +1,19 @@
-
+/*pattern be liked
+  2 4 6 8 10 
+ 12 14 16 18 20 
+ 22 24 26 28 30 
+ 32 34 36 38 40 
+ 42 44 46 48 50 */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+           
+            x=x+2;
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
