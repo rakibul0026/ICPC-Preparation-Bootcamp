dbaf168d590e76337ad32b011603bef963c3cc54
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 12:50:37 +0600

    Update p3.cpp

--- For loop, while loop, do_while loop/Nested for loop/p3.cpp
+++ For loop, while loop, do_while loop/Nested for loop/p3.cpp
@@ -1,1 +1,14 @@
 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<=10;i++){
+        for(int j=0;j<=10;j++){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+    }
+}
