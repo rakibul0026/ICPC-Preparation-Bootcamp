b23f2253825203444d3bb619e35bd6aef819890e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 12:21:32 +0600

    Update print smaller than one Element in array.cpp

--- For loop, while loop, do_while loop/print smaller than one Element in array.cpp
+++ For loop, while loop, do_while loop/print smaller than one Element in array.cpp
@@ -6,21 +6,18 @@ int main()
 {
     int a[6] = {3, 5, 2, 6, 4, 9};
 
-    for (int i = 0; i < 6; i++)
-    {
-        int x = a[i];
-        cout << x << "---->";
-
-        for (int j = 0; j < 6; j++)
-        {
-            if (a[j] < x)
-            {
-                cout << a[j] << " ";
+    for(int i=0;i<6;i++){
+        cout<<a[i]<<"---->";
+        for(int j=0;j<6;j++){
+            if(a[i]>a[j]){
+                cout<<a[j]<<" ";
             }
+          
         }
-        cout << endl;
+          cout<<endl;
     }
 
+   
     return 0;
 }
 
