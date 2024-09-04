e87fa2aa18b1efd5c3fdf51c0f9a9643bc95b031
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 12:35:17 +0600

    Update p1.cpp

--- For loop, while loop, do_while loop/Nested for loop/p1.cpp
+++ For loop, while loop, do_while loop/Nested for loop/p1.cpp
@@ -1,1 +1,16 @@
-
+/*pattarn like->
+  00000
+  11111
+  22222
+  33333
+  44444
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
