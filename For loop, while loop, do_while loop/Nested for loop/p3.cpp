988c6aa138108aa3a32498ff4e78c8e7b7196f94
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-04 12:52:43 +0600

    Update p3.cpp

--- For loop, while loop, do_while loop/Nested for loop/p3.cpp
+++ For loop, while loop, do_while loop/Nested for loop/p3.cpp
@@ -1,11 +1,17 @@
-
-
-
+  /*pattarn like->
+      0246
+      0246
+      0246
+      0246
+      0246
+      0246
+      0246 */
+      
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    for(int i=0;i<=10;i++){
-        for(int j=0;j<=10;j++){
+    for(int i=0;i<=6;i++){
+        for(int j=0;j<=6;j++){
             cout<<j;
             j++;
         }
