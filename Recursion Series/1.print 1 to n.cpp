5d7c8cbe50edc595d0e4be27eb2381d196042354
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-27 12:01:28 +0600

    Update 1.print 1 to n.cpp

--- Recursion Series/1.print 1 to n.cpp
+++ Recursion Series/1.print 1 to n.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void print(int n) {
+    if(n == 1) {  // Base case
+        cout << 1 << " "; 
+        return; 
+    }
+    print(n - 1);  // Recursive call
+    cout << n << " "; 
+}
+
+int main() {
+    while(1) {
+        int n;
+        cin >> n;
+        print(n);
+        cout << endl;  // Output formatting
+    }
+}
