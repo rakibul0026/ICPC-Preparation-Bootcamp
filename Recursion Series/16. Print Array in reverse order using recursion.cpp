71bda9297581fe55bbdbaf2cb80983daad87aa57
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-29 00:14:43 +0600

    Update 16. Print Array in reverse order using recursion.cpp

--- Recursion Series/16. Print Array in reverse order using recursion.cpp
+++ Recursion Series/16. Print Array in reverse order using recursion.cpp
@@ -1,1 +1,26 @@
+// C++ Program to print array using
+// recursion
+#include <iostream>
+using namespace std;
+
+// recursive function to print array
+void pArray(int* arr, int n)
+{
+    // base condition
+    if (n == 0) {
+        return;
+    }
+    // recursive call
+    cout << arr[n - 1] << ' ';
+    pArray(arr, n - 1);  
+}
+
+int main()
+{
+   
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    // calling function
+    pArray(arr, 5);
+    return 0;
+}
 
