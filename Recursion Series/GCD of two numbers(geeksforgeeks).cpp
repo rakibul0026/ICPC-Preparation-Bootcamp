e0018d8ef78a97d081c9fec894e143e68014184e
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-29 15:47:05 +0600

    Create GCD of two numbers(geeksforgeeks).cpp

---
+++ Recursion Series/GCD of two numbers(geeksforgeeks).cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.gcd(a, b);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
