10627cc9624c231848047302c5f1d5cef42e5b80
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-01 15:01:15 +0600

    Update binary search with recursion.cpp

--- Recursion Series/binary search with recursion.cpp
+++ Recursion Series/binary search with recursion.cpp
@@ -1,1 +1,23 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+bool isSorted(int *arr,int size){
+    //base
+    if(size==0 ||size==1)
+    return true;
+    if(arr[0]>arr[1]){
+        return false;
+    }else {
+    bool remainingpart=isSorted(arr+1,size-1);
+    return remainingpart;
+    }
+
+}
+int main(){
+   int arr[5]={2,4,12,8,9};
+   int size=5;
+
+   bool ans=isSorted(arr,size);
+   cout<<ans;
+
+}
