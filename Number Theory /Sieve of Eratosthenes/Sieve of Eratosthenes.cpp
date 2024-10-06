513af8eb1e5bc9de0fd648271613f48d2c3b4061
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-06 11:29:19 +0600

    Update Sieve of Eratosthenes.cpp

--- Number Theory /Sieve of Eratosthenes/Sieve of Eratosthenes.cpp
+++ Number Theory /Sieve of Eratosthenes/Sieve of Eratosthenes.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sieve(int N) {
+    vector<bool> isPrime(N+1, true);
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not primes
+
+    for(int i = 2; i * i <= N; i++) {
+        if(isPrime[i]) {
+            for(int j = i * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    // Output all prime numbers up to N
+    for(int i = 2; i <= N; i++) {
+        if(isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    sieve(N);
+}
 
