7078cde6164cb72cba3427867f9eea9c745f7d6f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-06 15:46:56 +0600

    Update C. Prime factorization(codeforce).cpp

--- Number Theory / Prime Factorization/C. Prime factorization(codeforce).cpp
+++ Number Theory / Prime Factorization/C. Prime factorization(codeforce).cpp
@@ -1,3 +1,28 @@
+/*C.                                            Prime factorization
+                                             time limit per test3 seconds
+                                          memory limit per test256 megabytes
+You are given a positive integer n. Output its prime factorization.
+
+If n = a1b1 a2b2 ... akbk (bi > 0), where ak are prime numbers, the output of your program should look as 
+follows: a1*...*a1*a2*...*a2*...*ak*...*ak, where the factors are ordered in non-decreasing order, and
+each factor ai is printed bi times.
+
+Input
+The only line of input contains an integer n (2 ≤ n ≤ 10000).
+
+Output
+Output the prime factorization of n, as described above.
+
+Examples
+Input
+245
+Output
+5*7*7
+Input
+19
+Output
+19                     */
+
 #include <bits/stdc++.h>
 using namespace std;
 
