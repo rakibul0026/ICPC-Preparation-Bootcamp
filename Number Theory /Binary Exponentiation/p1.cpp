c4d38d671215628f698ccc6ccb90cc36dbf4a6e0
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-07 03:58:10 +0600

    Update p1.cpp

--- Number Theory /Binary Exponentiation/p1.cpp
+++ Number Theory /Binary Exponentiation/p1.cpp
@@ -1,1 +1,46 @@
+/*Logic->(2^5)
+   
+         base         power        ans
+    1->  2              5           1      if power is odd -> ans = (ans*base) 
+                                             and decrease power
+
+    2->  2              4           2      if power is Even -> ans = (base*base) 
+                                             and  power=power/2
+         
+         mod Use ->mod use  because when power is a large value value
+                   it does not work properly
+
+ */  
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+int func(int base, int power, int mod)
+{
+    int ans = 1;
+    while(power)
+    {
+        if(power%2==1)
+        {
+          ans = (ans*base)%mod;
+          power--;
+        }
+        else 
+        {
+          base = (base*base)%mod;
+          power = power/2;
+        }
+    }
+    return ans;
+}
+ 
+signed main()
+{
+    int base,power,mod=1e9;
+    cin>>base>>power;
+    cout<<func(base,power,mod)<<endl;
+    return 0;
+}
+ 
 
