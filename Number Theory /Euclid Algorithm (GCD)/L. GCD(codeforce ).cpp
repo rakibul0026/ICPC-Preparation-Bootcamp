453f7d0ddc09f1dab9c98278e586e2d3c88556d1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-07 06:04:32 +0600

    Update L. GCD(codeforce ).cpp

--- Number Theory /Euclid Algorithm (GCD)/L. GCD(codeforce ).cpp
+++ Number Theory /Euclid Algorithm (GCD)/L. GCD(codeforce ).cpp
@@ -59,4 +59,25 @@ gcd(54,24) = 6.
 
 
 */
+//code
+
+#include <iostream>
+using namespace std;
+int gcd(int n, int m)
+{
+    if (n % m == 0)
+        return m;
+    return gcd(m, n % m);
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+   cout<< gcd(N, M);
+
+
+    return 0;
+}
+
 
