c2de5e628426a63ca512fc3af85496c66d9eb490
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-07 06:01:21 +0600

    Update Euclid Algorithm (GCD).cpp

--- Number Theory /Euclid Algorithm (GCD)/Euclid Algorithm (GCD).cpp
+++ Number Theory /Euclid Algorithm (GCD)/Euclid Algorithm (GCD).cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+using namespace std;
+int gcd(int n, int m)
+{
+    if (n % m == 0)
+        return m;
+    return gcd(m, n % m);
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+   cout<< gcd(N, M);
+
+
+    return 0;
+}
+
