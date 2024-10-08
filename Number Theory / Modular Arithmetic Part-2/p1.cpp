d0c37b3cfed3ba8f2104160ef5348af0c3b6a164
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-08 16:54:21 +0600

    Update p1.cpp

--- Number Theory / Modular Arithmetic Part-2/p1.cpp
+++ Number Theory / Modular Arithmetic Part-2/p1.cpp
@@ -1,3 +1,19 @@
+/*
+100101010101010101010101010101010101010101010%5
+string =n
+int x=ch-'0
+ ans=0;
+  for(int i=0;i<s.size();i++)
+    {
+        ans=ans*10 + (s[i]-'0');
+        ans=ans%m;
+    }
+    if(ans%m==0)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+
+
+*/
 #include<bits/stdc++.h>
 using  namespace  std;
 void div(string s,int m)
