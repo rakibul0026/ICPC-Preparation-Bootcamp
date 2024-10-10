b75cecd944e1a58dd18352bc4ddaa3bc0e187456
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-11 01:48:46 +0600

    Update 1: Minimum number of Coins(geeksforgeeks).cpp

--- Greedy Algorithm/1: Minimum number of Coins(geeksforgeeks).cpp
+++ Greedy Algorithm/1: Minimum number of Coins(geeksforgeeks).cpp
@@ -1,1 +1,47 @@
-(geeksforgeeks)
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> minPartition(int N)
+    {
+       int currency[]={2000,500,200,100,50,20,10,5,2,1};
+       int notes=0,i=0;
+       vector<int>ans;
+       while(N)
+       {
+           notes=N/currency[i];
+           while(notes--){
+               ans.push_back(currency[i]);
+           }
+           N%=currency[i];
+           i++;
+       }
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> numbers = ob.minPartition(N);
+        for(auto u: numbers)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
