5ab8b3c25e9e130c5a317f6017bd8dca34d64eda
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 00:30:21 +0600

    Update 2.Find the pair in Array Whose sum is closest to X.cpp

--- Two Pointer Full Series/2.Find the pair in Array Whose sum is closest to X.cpp
+++ Two Pointer Full Series/2.Find the pair in Array Whose sum is closest to X.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+
+void solve(int ar[],int n, int x)
+{
+    int l=0;
+    int r=n-1;
+    int ind1,ind2;
+    int dif=INT_MAX;
+    while(l<r)
+    {
+        int sum=ar[l]+ar[r];
+        if(abs(sum-x)<dif)
+        {
+            ind1=l;
+            ind2=r;
+            dif=abs(sum-x);
+        }
+        if(sum>x)r--;
+        else l++;
+    }
+    cout<<"("<<ar[ind1]<<" "<<ar[ind2]<<")  = "<< ar[ind1]+ar[ind2]<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n+3];
+    for(int i=0;i<n;i++)cin>>ar[i];
+    sort(ar,ar+n);
+    int x;
+    cin>>x;
+    solve(ar,n,x);
+}
 
