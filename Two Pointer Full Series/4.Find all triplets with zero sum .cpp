47bc90d94d1969cf650f9392db3a0f209903b773
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 00:52:01 +0600

    Update 4.Find all triplets with zero sum .cpp

--- Two Pointer Full Series/4.Find all triplets with zero sum .cpp
+++ Two Pointer Full Series/4.Find all triplets with zero sum .cpp
@@ -1,1 +1,30 @@
-
+#include<bits/stdc++.h>
+using  namespace  std;
+ 
+void solve(int ar[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        unordered_set<int>s;
+        for(int j=i+1;j<n;j++)
+        {
+            int x = -(ar[i]+ar[j]);
+            if(s.find(x)!=s.end())
+            {
+                cout<<"("<<x<<","<<ar[i]<<","<<ar[j]<<")"<<endl;
+            }
+            else{
+                s.insert(ar[j]);
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n+3];
+    for(int i=0;i<n;i++)cin>>ar[i];
+    solve(ar,n);
+}
+ 
