af995459ddde291c7f351ce2fda7b868025df7b2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 00:06:08 +0600

    Update 1.Find the pair whose sum is equal to x.cpp

--- Two Pointer Full Series/1.Find the pair whose sum is equal to x.cpp
+++ Two Pointer Full Series/1.Find the pair whose sum is equal to x.cpp
@@ -1,1 +1,42 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int val1, val2;
+bool solve(int ar[], int x, int n)
+{
+    int i = 0;
+    int j = n - 1;
+    while (i <= j)
+    {
+        int sum = ar[i] + ar[j];
+        if (sum == x)
+        {
+            val1 = ar[i];
+            val2 = ar[j];
+            return true;
+        }
+        if (sum > x)
+            j--;
+        else
+            i++;
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int ar[n + 3];
+    for (int i = 0; i < n; i++)
+        cin >> ar[i];
+    sort(ar, ar + n);
+    int x;
+    cin >> x;
+    bool ans = solve(ar, x, n);
+    if (ans == true)
+    {
+        cout << "Yes" << endl;
+        cout << val1 << " " << val2 << endl;
+    }
+    else
+        cout << "no" << endl;
+}
