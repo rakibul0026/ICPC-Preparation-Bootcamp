e732396e4a7f00a40b86d3a35e14ffa1e71f20ad
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 12:43:20 +0600

    Update A. Sereja and Dima(Codeforces Solution).cpp

--- Two Pointer Full Series/A. Sereja and Dima(Codeforces Solution).cpp
+++ Two Pointer Full Series/A. Sereja and Dima(Codeforces Solution).cpp
@@ -1,1 +1,60 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n+3];
+    for(int i=0;i<n;i++)cin>>ar[i];
+    int sereja=0,dima=0;
+    int l=0;
+    int r=n-1;
+    int z=0;
+    while(l<=r)
+    {
+        if(z%2==0)
+        {
+            if(ar[l]>ar[r])
+            {
+                sereja+=ar[l];
+                l++;
+            }
+            else{
+                sereja+=ar[r];
+                r--;
+            }
 
+        }
+        else{
+            if(ar[l]>ar[r])
+            {
+                dima+=ar[l];
+                l++;
+            }
+            else{
+                dima+=ar[r];
+                r--;
+            }
+
+        }
+        z++;
+    }
+    cout<<sereja<<" "<<dima<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
