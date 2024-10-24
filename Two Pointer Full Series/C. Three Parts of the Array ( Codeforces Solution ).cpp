07e9806f752db68610f57cf06af2a7152086e9ea
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-25 00:54:58 +0600

    Update C. Three Parts of the Array ( Codeforces Solution ).cpp

--- Two Pointer Full Series/C. Three Parts of the Array ( Codeforces Solution ).cpp
+++ Two Pointer Full Series/C. Three Parts of the Array ( Codeforces Solution ).cpp
@@ -1,1 +1,31 @@
-
+#include<bits/stdc++.h>
+using  namespace  std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n+4];
+    for(int i=0;i<n;i++)cin>>ar[i];
+    long long int sum1=0,sum2=0,ans=0;
+    int l=0;
+    int r=n-1;
+    while(l<=r)
+    {
+        if(sum1<sum2)
+        {
+            sum1+=ar[l];
+            l++;
+        }
+        else{
+            sum2+=ar[r];
+            r--;
+        }
+        if(sum1==sum2)ans=sum1;
+    }
+    cout<<ans<<endl;
+}
+ 
+ 
+ 
+ 
+ 
