a9d88644563f3b3b1f5ebd71fa5dd02c6a40c031
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 12:39:59 +0600

    Update A. Favorite Sequence (Codeforces Solution ).cpp

--- Two Pointer Full Series/A. Favorite Sequence (Codeforces Solution ).cpp
+++ Two Pointer Full Series/A. Favorite Sequence (Codeforces Solution ).cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int ar[n + 3];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+        }
+        int i = 0;
+        int j = n - 1;
+
+        vector<int> v;
+        while (i < j)
+        {
+            v.push_back(ar[i]);
+            v.push_back(ar[j]);
+            i++;
+            j--;
+        }
+        if (n % 2 == 1)
+            v.push_back(ar[i]);
+        for (int i = 0; i < n; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
+}
