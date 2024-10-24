4a46535d4a10f36aa59bbdce63bbbb7e12786f23
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-24 13:08:38 +0600

    Update 7.Find a triplet such that sum of two equals to third element .cpp

--- Two Pointer Full Series/7.Find a triplet such that sum of two equals to third element .cpp
+++ Two Pointer Full Series/7.Find a triplet such that sum of two equals to third element .cpp
@@ -1,1 +1,34 @@
-
+#include<bits/stdc++.h>
+using  namespace  std;
+ 
+void solve(int ar[],int n)
+{
+    for(int i=n-1;i>=0;i--)
+    {
+        int x =ar[i];
+        int l=0;
+        int r=i-1;
+        while(l<r)
+        {
+            int sum=ar[l]+ar[r];
+            if(sum==x)
+            {
+                cout<<"("<<x<<","<<ar[l]<<","<<ar[r]<<")"<<endl;
+                return;
+            }
+            if(sum>x)r--;
+            else l++;
+        }
+    }
+    cout<<"No Such Triplates exists"<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n+3];
+    for(int i=0;i<n;i++)cin>>ar[i];
+    sort(ar,ar+n);
+    solve(ar,n);
+}
+ 
