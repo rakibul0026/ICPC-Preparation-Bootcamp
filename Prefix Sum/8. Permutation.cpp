5e8adc0adc5d4dfa7da4da9357d1dfaa561962dc
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-29 01:19:03 +0600

    Update 8. Permutation.cpp

--- Prefix Sum/8. Permutation.cpp
+++ Prefix Sum/8. Permutation.cpp
@@ -1,1 +1,27 @@
-
+ 
+#include<bits/stdc++.h>
+using  namespace  std;
+ 
+void print(vector<int>&v,int n)
+{
+    for(int i=0;i<n;i++)cout<<v[i]<<" ";
+    cout<<endl;
+}
+ 
+int main()
+{
+   vector<int>v;
+   int n;
+   cin>>n;
+   int x;
+   for(int i=0;i<n;i++)
+   {
+       cin>>x;
+       v.push_back(x);
+   }
+ 
+   while(next_permutation(v.begin(),v.end()))
+   {
+       print(v,n);
+   }
+}
