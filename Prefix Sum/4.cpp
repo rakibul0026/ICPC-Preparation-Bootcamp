f408125e68d502d7d35ca7ba14e4d98c58952654
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-28 14:59:33 +0600

    Update 4.cpp

--- Prefix Sum/4.cpp
+++ Prefix Sum/4.cpp
@@ -1,1 +1,36 @@
-
+#include<bits/stdc++.h>
+using  namespace  std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int ar[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>ar[i][j];
+        }
+    }
+ 
+    int pre[n][m];
+    pre[0][0]=ar[0][0];
+    for(int i=1;i<m;i++)pre[0][i]=pre[0][i-1]+ar[0][i];
+    for(int i=1;i<n;i++)pre[i][0]=pre[i-1][0]+ar[i][0];
+ 
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<m;j++)
+        {
+            pre[i][j]=pre[i][j-1]+pre[i-1][j]+ar[i][j]-pre[i-1][j-1];
+        }
+    }
+ 
+ 
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)cout<<pre[i][j]<<" ";
+        cout<<endl;
+    }
+ 
+}
