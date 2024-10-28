f04fb0933554d03911725f5425af03307dbcf6af
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-28 12:38:51 +0600

    Update 3.Difference Array.cpp

--- Prefix Sum/3.Difference Array.cpp
+++ Prefix Sum/3.Difference Array.cpp
@@ -1,3 +1,36 @@
+/*
+      n=7
+      index=0 1 2 3 4 5 6 
+      array=2 3 4 5 6 7 8
+
+      query=3
+        l  r  v
+
+     / case 1: 0 3 2
+       array=4 5 6 7 6 7 8
+       
+      case 2:1 5 1
+      array=4 6 7 8 7 8 8
+      
+      case 3: 4 6 3 
+      array= 4 6 7 8 10 11 11  /--->This part are  worst tecknique
+
+   logic  part--->  dif[n+1]={0};
+           dif =0 0 0 0 0 0 0    operation after dif= 2 1 0 0 -2 0 -1
+                              it prefix  is = 2 2 2 2 2 2 0
+                              then  it add the given array value
+                                array= 4 5 6 7 6 7 8
+       
+     query =3
+      {
+     case: 0 3 2
+      dif[l]+=v
+      dif[r+1]-=v
+      }
+      
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
